c4b5f3988a4e780e3bd78747d50a39ce7c15ae6c
Starraccon <emilly_santana@estudante.sesisenai.org.br>
2024-08-28 16:41:27 -0300

    Arquivo original

---
+++ python/teste.py
@@ -0,0 +1,12 @@
+codigo = 10
+salario = 1500.00
+nome = 'Zé'
+situação = True
+
+tipo = type(codigo)
+
+
+print(codigo)
+print(tipo)
+print("Código:",codigo, "nome: ", nome, "o salário atual é de: R$ ", salario)
+print("Código:" +str(codigo)+ "nome: " + nome + "o salário atual é de: R$ " + str(salario)) 
---
+++ python/teste4.py
@@ -0,0 +1,10 @@
+codigo = int(input("Digite o cófigo do funcionário "))
+nome = input("Digite o nome do funcionário ")
+salario = float(input('Informe o salário '))
+ativo = True
+
+
+print("Código: %d "% codigo)
+print("Nome: %s "% nome)
+print("Salário: %.2f "% salario)
+print("Ativo: %r "% ativo)
